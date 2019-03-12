16cd667c5bff691e003acf14a414acfe84a8a9dc
nepston <maratb2010@mail.ru>
2019-03-12 04:05:04 +0300

    comments & subscriptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,15 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user_can_edit?
 
+  def current_user_can_edit?(model)
+    user_signed_in? && (
+    model.user == current_user ||
+        (model.try(:event).present? && model.event.user == current_user)
+    )
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update,
                                       keys: [:password, :password_confirmation, :current_password])
-  end
-
-  def current_user_can_edit?(event)
-    user_signed_in? && event.user == current_user
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,31 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :events
+  has_many :comments, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
+
+
 
   validates :name, presence: true, length: {maximum: 35}
 
+  validates :email, length: {maximum: 255}
+  validates :email, uniqueness: true
+  validates :email, format: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   before_validation :set_name, on: :create
+
+  after_commit :link_subscriptions, on: :create
 
   private
 
   def set_name
     self.name = "Пользователь №#{rand(777)}" if self.name.blank?
+  end
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email)
+        .update_all(user_id: self.id)
   end
 end
