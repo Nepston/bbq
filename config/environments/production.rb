bceded7d35b290cde99b0195736242c662ca6f76
nepston <maratb2010@mail.ru>
2019-03-30 21:28:00 +0300

    changed mailer config to MailGun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,12 +96,12 @@ Rails.application.configure do
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-      :address              => 'smtp.sendgrid.net',
-      :port                 => '587',
+      :address              => ENV['MAILGUN_SMTP_SERVER'],
+      :port                 => ENV['MAILGUN_SMTP_PORT'],
       :authentication       => :plain,
-      :user_name            => ENV['SENDGRID_USERNAME'],
-      :password             => ENV['SENDGRID_PASSWORD'],
-      :domain               => 'heroku.com',
+      :user_name            => ENV['MAILGUN_SMTP_LOGIN'],
+      :password             => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain               => ENV['MAILGUN_DOMAIN'],
       :enable_starttls_auto => true
   }
 end
